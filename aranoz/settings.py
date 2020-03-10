10d42bfda4cafd52bbaba3802017bc7aa4c66b58
ElimuStuart <stuartelimu@gmail.com>
2020-03-11 02:56:31 +0300

    run collectstatic command

--- aranoz/settings.py
+++ aranoz/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_root')
 ]
