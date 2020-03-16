dd59feab48501fcb3b53a063af498878058e53f3
ElimuStuart <stuartelimu@gmail.com>
2020-03-17 00:39:11 +0300

    Add settings for media files

--- aranoz/urls.py
+++ aranoz/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
