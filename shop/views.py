dddbf92e21a286ceb0301900e10557ba3a7e768d
ElimuStuart <stuartelimu@gmail.com>
2020-03-17 00:58:35 +0300

    add IndexView and ItemList views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from shop.models import Item
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+   
+
+class ItemList(ListView):
+    model = Item
+    template_name = 'shop/category.html'
+    context_object_name = 'items'
