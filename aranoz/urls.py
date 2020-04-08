3dfd9ed57a93b3e5e619a73fdbe68b5fe17514f2
ElimuStuart <stuartelimu@gmail.com>
2020-04-08 23:12:21 +0300

    update index.html

--- aranoz/urls.py
+++ aranoz/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+
+from .views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('shop/', include('shop.urls')),
+    path('', IndexView.as_view()),
 ]
 
 if settings.DEBUG:
