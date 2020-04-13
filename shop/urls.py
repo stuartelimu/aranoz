ac143f8bf242270210f7cf904670231738b0c575
ElimuStuart <stuartelimu@gmail.com>
2020-04-13 22:48:48 +0300

    move cart url to top of the list to fix slug conflict

--- shop/urls.py
+++ shop/urls.py
@@ -5,7 +5,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.ItemList.as_view(), name='item_list'),
+    path('cart/', views.CartView.as_view(), name='cart'),
     path('<slug:slug>/', views.ItemDetail.as_view(), name='item_detail'),
     path('add/<slug:slug>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.CartView.as_view(), name='cart'),
 ]
