45316ea05b74e1dcec8bf737c4911efb91aa4097
ElimuStuart <stuartelimu@gmail.com>
2020-04-14 23:01:50 +0300

    add cart quantity increment functionality

--- shop/urls.py
+++ shop/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
     path('<slug:slug>/', views.ItemDetail.as_view(), name='item_detail'),
     path('add/<slug:slug>/', views.add_to_cart, name='add_to_cart'),
+    path('add_single_item_to_cart/<slug:slug>/', views.add_single_item_to_cart, name='add_single_item_to_cart'),
+
 ]
